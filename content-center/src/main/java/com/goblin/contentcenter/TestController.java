fcdd552a596cf1f5765893d15d67eb5bf7712d06
slpgoblin <cvmslh@163.com>
2019-12-07 23:25:01 +0800

    新增一些 sentinel的方法

--- content-center/src/main/java/com/goblin/contentcenter/TestController.java
+++ content-center/src/main/java/com/goblin/contentcenter/TestController.java
@@ -1,8 +1,16 @@
 package com.goblin.contentcenter;
 
+import com.alibaba.csp.sentinel.Entry;
+import com.alibaba.csp.sentinel.SphU;
+import com.alibaba.csp.sentinel.Tracer;
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.context.ContextUtil;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.goblin.contentcenter.domain.dto.user.UserDTO;
 import com.goblin.contentcenter.feignclient.TestBlogFeignClient;
 import com.goblin.contentcenter.feignclient.TestUserCenterFeignClient;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
@@ -20,6 +28,7 @@ import java.util.Map;
  **/
 @RestController
 @RequestMapping("test")
+@Slf4j
 public class TestController {
 
     @Resource
@@ -48,5 +57,48 @@ public class TestController {
     @GetMapping("test-blog")
     public String testBlog(){
         return testBlogFeignClient.index();
+    }
+
+    @GetMapping("test-hot")
+    @SentinelResource("hot")
+    public String testHot(
+            @RequestParam(required = false) String a,
+            @RequestParam(required = false) String b){
+        return a + " " + b;
+    }
+
+    @GetMapping("/test-sentinel-api")
+    public String testSentinelAPI(
+            @RequestParam(required = false) String a) {
+
+        String resourceName = "test-sentinel-api";
+        ContextUtil.enter(resourceName, "test-wfw");
+
+        // 定义一个sentinel保护的资源，名称是test-sentinel-api
+        Entry entry = null;
+        try {
+
+            entry = SphU.entry(resourceName);
+            // 被保护的业务逻辑
+            if (StringUtils.isBlank(a)) {
+                throw new IllegalArgumentException("a不能为空");
+            }
+            return a;
+        }
+        // 如果被保护的资源被限流或者降级了，就会抛BlockException
+        catch (BlockException e) {
+            log.warn("限流，或者降级了", e);
+            return "限流，或者降级了";
+        } catch (IllegalArgumentException e2) {
+            // 统计IllegalArgumentException【发生的次数、发生占比...】
+            Tracer.trace(e2);
+            return "参数非法！";
+        } finally {
+            if (entry != null) {
+                // 退出entry
+                entry.exit();
+            }
+            ContextUtil.exit();
+        }
     }
 }
