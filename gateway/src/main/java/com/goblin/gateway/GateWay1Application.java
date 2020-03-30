a7a95bd487bfb8714d5df2e0c79194fe4feb7478
slpgoblin <cvmslh@163.com>
2020-03-30 13:14:11 +0800

    整合gateway

---
+++ gateway/src/main/java/com/goblin/gateway/GateWay1Application.java
@@ -0,0 +1,13 @@
+package com.goblin.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GateWay1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GateWay1Application.class, args);
+    }
+
+}
